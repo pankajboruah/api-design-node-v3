789c4ab16d8337c14e1fb86884dc2d441a39536b
Pankaj Boruah <pankaj.boruah@upgrad.com>
2021-06-27 20:28:48 +0530

    set basic routes

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,40 @@ import { Router } from 'express'
 
 const router = Router()
 
+const controller = (req, res) => {
+  res.send({ message: 'hello world' })
+}
+// router.get('/api/item', (req, res) => {
+//   res.send({ message: 'items' })
+// })
+
+// router.post('/api/item', (req, res) => {
+//   res.send({ message: 'post a new item' })
+// })
+
+// router.get('/api/item/:id', (req, res) => {
+//   res.send({ message: `get item with id: ${req.params.id}` })
+// })
+
+// router.put('/api/item/:id', (req, res) => {
+//   res.send({ message: `update item with id: ${req.params.id}` })
+// })
+
+// router.delete('/api/item/:id', (req, res) => {
+//   res.send({ message: `delete item with id: ${req.params.id}` })
+// })
+
+// equivalent to /api/item
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+// equivalent to /api/item/:id
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
+
 export default router
