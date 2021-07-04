3445522ec5560c02a5408506d71dc2d36c575ff5
Pankaj Boruah <pankaj.boruah@upgrad.com>
2021-07-04 13:50:30 +0530

    setup jwt

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,63 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  if (!email && !password) {
+    return res.status(400).send({ message: 'Email and password are required' })
+  }
+  try {
+    const user = await User.create(req.body)
+    const userAuthToken = newToken(user)
+    return res.status(201).send({ token: userAuthToken })
+  } catch (err) {
+    console.error(err)
+    return res.status(401).end()
+  }
+}
 
-export const signin = async (req, res) => {}
+export const signin = async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  if (!email && !password) {
+    return res.status(400).send({ message: 'Email and password are required' })
+  }
+  try {
+    const user = await User.findOne({ email: email }).exec()
+    if (!user) {
+      return res.status(401).send({ message: 'User not found' })
+    }
+    const match = await user.checkPassword(password)
+    if (!match) {
+      return res.status(401).send({ message: 'Invalid password' })
+    }
+    const token = newToken(user)
+    return res.status(201).send({ token })
+  } catch (err) {
+    console.error(err)
+    return res.status(401).end()
+  }
+}
 
 export const protect = async (req, res, next) => {
-  next()
+  const token = req.headers.authorization
+  if (!token) {
+    return res.status(401).end()
+  }
+  if (!token.includes('Bearer')) {
+    return res.status(401).end()
+  }
+  try {
+    const payload = await verifyToken(token.split(' ')[1])
+    const user = await User.findById(payload.id)
+      .select('-password')
+      .lean()
+      .exec()
+    req.user = user
+    next()
+  } catch (e) {
+    console.error(e)
+    return res.status(401).end()
+  }
 }
